b46552e92d69056e7a7aa28f58832f8e44d5b946
Muhammad Tawsif Sazid <32050503+tawsifsazid@users.noreply.github.com>
2019-10-15 18:14:36 +0600

    Add files via upload

---
+++ Start.cpp
@@ -0,0 +1,32 @@
+#define IO std::ios::sync_with_stdio(false);
+#include <bits/stdc++.h>
+#define pii acos(-1)
+#define ll long long
+#define pare pair<int,int>
+#define eps 1e-9
+#define maxx 60006
+#define read(type) readInt<type>()
+
+
+/****** Template of some basic operations *****/
+
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+
+/**********************************************/
+
+using namespace std;
+
+///dir array for Queen int dxq[10] = {-1,-1,-1,1,1,1,0,0};
+///dir array for Queen int dyq[10] = {-1,0,1,-1,0,1,-1,1};
+///dir array for knight int dxk[10] = {-2,-2,-1,-1,1,1,2,2};
+///dir array for knight int dyk[10] = {1,-1,-2,2,-2,2,-1,1};
+int cases,ct = 0;
+
+int main(){
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+
+
+
+}
